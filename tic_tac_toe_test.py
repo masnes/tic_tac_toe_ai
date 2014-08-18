7baa789afdf7eb865450a1767991b0e3faf9d6aa
Michael Asnes <mike.asnes@gmail.com>
2014-08-17 20:59:55 -0600

    added messages to beginning, end of tests

--- tic_tac_toe_test.py
+++ tic_tac_toe_test.py
@@ -472,9 +472,12 @@ def debug_check_list_for_almost_n_in_a_row():
 
 
 if __name__ == "__main__":
+    print("starting tests")
     debug_node()
     debug_check_board_full_n_in_a_row()
     debug_count_values()
     debug_swap_players()
     debug_WhosTurnGenerator()
     debug_check_list_for_almost_n_in_a_row()
+    print("all tests finished. If there is no other output here, "
+          "everything passed!")
